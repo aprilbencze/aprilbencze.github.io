47e7bd830ad8253b3c514ac30744528c01e5de37
Peter Thicke <peterbthicke@gmail.com>
2015-10-19 09:27:01 -0700

    removed jquery plugins and wrote simple image display

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,8 +1,28 @@
 $(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    slideshow: false,
-    directionNave:false
-  });
+
+  $("#slide2").hide();
+  $("#slide3").hide();
+
+  function hide () {
+    $("#slide1").hide();
+    $("#slide2").hide();
+    $("#slide3").hide();
+  }
+
+
+  $("#number1").on('click', function () {
+    hide();
+    $("#slide1").show();
+  })
+
+  $("#number2").on('click', function () {
+    hide();
+    $("#slide2").show();
+  })
+
+  $("#number3").on('click', function () {
+    hide();
+    $("#slide3").show();
+  })
 });
 
